59dc14f709c7833544605983133134a2f2251494
memirgurbuz44-source <memirgurbuz44@gmail.com>
2026-08-27 16:35:01 +0300

    Update and rename ResistanceVignetteClient.java to QmcxozelmodClient.java

--- src/main/java/com/example/addon/client/QmcxozelmodClient.java
+++ src/main/java/com/example/addon/client/QmcxozelmodClient.java
@@ -4,18 +4,18 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.util.Identifier;
 
-public class ResistanceVignetteClient implements ClientModInitializer {
+public class QmcxozelmodClient implements ClientModInitializer {
 
     private static final Identifier VIGNETTE_TEXTURE = Identifier.of("minecraft", "textures/misc/vignette.png");
 
     @Override
     public void onInitializeClient() {
-        HudRenderCallback.EVENT.register((drawContext, tickDelta) -> {
+        HudRenderCallback.EVENT.register((drawContext, renderTickCounter) -> {
             MinecraftClient client = MinecraftClient.getInstance();
             if (client.player == null || client.options.hudHidden) return;
 
@@ -29,7 +29,7 @@ public class ResistanceVignetteClient implements ClientModInitializer {
                 float r = 0.0f, g = 0.0f, b = 0.0f, alpha = 0.0f;
 
                 if (duration > 100) { 
-                    // Mor renk (Efekt aktifken)
+                    // Mor renk (Efekt süresince)
                     r = 0.5f; g = 0.0f; b = 0.5f; alpha = 0.7f;
                 } else if (duration > 0) {
                     // Kırmızı yanıp sönme (Son 5 saniye)
@@ -40,19 +40,13 @@ public class ResistanceVignetteClient implements ClientModInitializer {
                     }
                 }
 
-                RenderSystem.disableDepthTest();
-                RenderSystem.depthMask(false);
                 RenderSystem.enableBlend();
-                RenderSystem.setShader(GameRenderer::getPositionTexProgram);
                 RenderSystem.setShaderColor(r, g, b, alpha);
 
-                drawContext.drawTexture(VIGNETTE_TEXTURE, 0, 0, -90, 0.0F, 0.0F, width, height, width, height);
+                drawContext.drawTexture(VIGNETTE_TEXTURE, 0, 0, 0, 0.0F, 0.0F, width, height, width, height);
 
                 RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-                RenderSystem.defaultBlendFunc();
                 RenderSystem.disableBlend();
-                RenderSystem.depthMask(true);
-                RenderSystem.enableDepthTest();
             }
         });
     }
